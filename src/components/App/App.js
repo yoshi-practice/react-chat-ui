f8883c04568edf72411027f3b902af616937855b
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-12-15 17:24:35 +0900

    Add material components / props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,23 +2,25 @@ import * as React from 'react';
 // import logo from './logo.svg';
 import './App.scss';
 import Tweet from '../Tweet/Tweet';
+// import Tweet from '../Tweet/Tweet';
 
 function App() {
-  return (
-    <div>
-      <Tweet
-        icon="🌽"
-        displayName="もろこし太郎"
-        accountName="morokoshi"
-        content="今日も1日もろこしがうまい"
-      />
-      <Tweet
-        icon="🦐"
-        displayName="エビデンス"
-        accountName="evidence"
-        content="かにみそたべたい"
-      />
-    </div>
+  // return (
+  //   <div>
+  //     <Tweet
+  //       icon="🌽"
+  //       displayName="もろこし太郎"
+  //       accountName="morokoshi"
+  //       content="今日も1日もろこしがうまい"
+  //     />
+  //     <Tweet
+  //       icon="🦐"
+  //       displayName="エビデンス"
+  //       accountName="evidence"
+  //       content="かにみそたべたい"
+  //     />
+  //   </div>
+  return(<Tweet name="yoshi" content="Hello!"/>
   );
 }
 
--- src/components/Tweet/Tweet.js
+++ src/components/Tweet/Tweet.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import './Tweet.scss';
+import {
+  makeStyles
+} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 function Tweet(props) {
+    const classes = useStyles();
+
   return (
-    <div className="tweet">
-      <div className="icon-container">{props.icon}</div>
-      <div className="body-container">
-        <div className="status-display">
-          <span className="display-name">{props.displayName}</span>
-          <span className="account-name">@{props.accountName}</span>
-        </div>
-        <div className="content">{props.content}</div>
-      </div>
-    </div>
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://image.shutterstock.com/image-vector/sample-stamp-square-grunge-sign-260nw-1474408826.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.content}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    // <div className="tweet">
+    //   <div className="icon-container">{props.icon}</div>
+    //   <div className="body-container">
+    //     <div className="status-display">
+    //       <span className="display-name">{props.displayName}</span>
+    //       <span className="account-name">@{props.accountName}</span>
+    //     </div>
+    //     <div className="content">{props.content}</div>
+    //   </div>
+    // </div>
   );
 }
 
