0f7ae73564286cf0209cd87c410ec1e614280b88
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-12-25 12:53:33 +0900

    Add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './Footer.scss';
+
+function Footer(){
+    return(
+        <footer>
+            <p>footer content</p>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App/App';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Header />, document.getElementById('header'))
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
